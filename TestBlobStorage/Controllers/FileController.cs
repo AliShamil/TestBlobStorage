fe722289879d30c03d0a9e7931ac324e9ca21fa3
AliShamil <elisamilzade@gmail.com>
2023-10-09 20:48:30 +0400

    Add project files.

---
+++ TestBlobStorage/Controllers/FileController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using TestBlobStorage.Services;
+
+namespace TestBlobStorage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly IStorageManager _storageManager;
+
+        public FileController(IStorageManager storageManager)
+        {
+            _storageManager = storageManager;
+        }
+
+        [HttpGet("getUrl")]
+        public IActionResult GetUrl(string fileName)
+        {
+            try
+            {
+                var result = _storageManager.GetSignedUrl(fileName);
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return StatusCode((int)HttpStatusCode.NotFound);
+            }
+        }
+
+        [HttpPost("uploadAsync")]
+        public async Task<IActionResult> UploadFileAsync(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return BadRequest("File not selected or empty.");
+
+            try
+            {
+                using (var stream = file.OpenReadStream())
+                {
+                    var contentType = file.ContentType;
+                    
+                    var fileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+
+                    var uploadResult = await _storageManager.UploadFileAsync(stream, fileName, contentType);
+
+                    if (uploadResult)
+                        return Ok("File uploaded successfully.");
+                    else
+                        return StatusCode((int)HttpStatusCode.InternalServerError, "Failed to upload the file.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, $"An error occurred: {ex.Message}");
+            }
+        } 
+
+        [HttpPost("upload")]
+        public  IActionResult UploadFile(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return BadRequest("File not selected or empty.");
+
+            try
+            {
+                using (var stream = file.OpenReadStream())
+                {
+                    var contentType = file.ContentType;
+                    
+                    var fileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+
+                    var uploadResult =  _storageManager.UploadFile(stream, fileName, contentType);
+
+                    if (uploadResult)
+                        return Ok("File uploaded successfully.");
+                    else
+                        return StatusCode((int)HttpStatusCode.InternalServerError, "Failed to upload the file.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, $"An error occurred: {ex.Message}");
+            }
+        }
+
+        
+        [HttpDelete("deleteAsync")]
+        public async Task<IActionResult> DeleteFileAsync(string fileName)
+        {
+            try
+            {
+                var deleteResult = await _storageManager.DeleteFileAsync(fileName);
+
+                if (deleteResult)
+                    return Ok("File deleted successfully.");
+                else
+                    return StatusCode((int)HttpStatusCode.InternalServerError, "Failed to delete the file.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, $"An error occurred: {ex.Message}");
+            }
+        }       
+        [HttpDelete("delete")]
+        public IActionResult DeleteFile(string fileName)
+        {
+            try
+            {
+                var deleteResult =  _storageManager.DeleteFile(fileName);
+
+                if (deleteResult)
+                    return Ok("File deleted successfully.");
+                else
+                    return StatusCode((int)HttpStatusCode.InternalServerError, "Failed to delete the file.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, $"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ TestBlobStorage/Services/BlobStorageManager.cs
@@ -0,0 +1,66 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Sas;
+using Microsoft.Extensions.Options;
+using TestBlobStorage.Utilities;
+
+namespace TestBlobStorage.Services
+{
+    public class BlobStorageManager : IStorageManager
+    {
+        public readonly BlobStorageOptions _storageOptions;
+
+        public BlobStorageManager(IOptions<BlobStorageOptions> options)
+        {
+            _storageOptions = options.Value;
+        }
+
+        public bool DeleteFile(string fileName)
+        {
+            var serviceClient = new BlobServiceClient(_storageOptions.ConnectionString);
+            var containerClient = serviceClient.GetBlobContainerClient(_storageOptions.ContainerName);
+            var blobClient = containerClient.GetBlobClient(fileName);
+
+            return  blobClient.DeleteIfExists();
+        }
+
+        public async Task<bool> DeleteFileAsync(string fileName)
+        {
+            var serviceClient = new BlobServiceClient(_storageOptions.ConnectionString);
+            var containerClient = serviceClient.GetBlobContainerClient(_storageOptions.ContainerName);
+            var blobClient = containerClient.GetBlobClient(fileName);
+
+            return await blobClient.DeleteIfExistsAsync();
+        }
+
+        public string GetSignedUrl(string fileName)
+        {
+            var serviceClient = new BlobServiceClient(_storageOptions.ConnectionString);
+            var contaionerClient = serviceClient.GetBlobContainerClient(_storageOptions.ContainerName);
+            var blobClient = contaionerClient.GetBlobClient(fileName);
+
+            var signedUrl = blobClient.GenerateSasUri(BlobSasPermissions.Read, DateTime.Now.AddHours(2)).AbsoluteUri;
+
+            return signedUrl;
+        }
+
+        public bool UploadFile(Stream stream, string fileName, string contentType)
+        {
+            var serviceClient = new BlobServiceClient(_storageOptions.ConnectionString);
+            var containerClient = serviceClient.GetBlobContainerClient(_storageOptions.ContainerName);
+            var blobClient = containerClient.GetBlobClient(fileName);
+            
+            blobClient.Upload(stream);
+            return true;
+        }
+
+        public async Task<bool> UploadFileAsync(Stream stream, string fileName, string contentType)
+        {
+            var serviceClient = new BlobServiceClient(_storageOptions.ConnectionString);
+            var containerClient = serviceClient.GetBlobContainerClient(_storageOptions.ContainerName);
+            var blobClient = containerClient.GetBlobClient(fileName);
+
+            await blobClient.UploadAsync(stream);
+            return true;
+        }
+    }
+}
